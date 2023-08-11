07a4df945d7db1e63ff1bfc39601dc9dd1bd9651
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2023-08-11 09:44:58 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Homepage from './pages/Homepage'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+import Header from './components/Header/Header'
+import SideBar from './components/SideBar/Sidebar'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Customer from './pages/Customer/Customer'
+import CustomerProfile from './pages/CustomerProfile/CustomerProfile'
 
 function App() {
 
   return (
     <>
-     <Homepage/>
+
+      <Router>
+        <Header />
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="customer" element={<Customer />} />
+          <Route path="customer-profile" element={<CustomerProfile />} />
+        </Routes>
+      </Router>
+
+
     </>
   )
 }
